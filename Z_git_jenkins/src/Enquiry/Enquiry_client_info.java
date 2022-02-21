b4ee4893462fca7c56e3ea592ca1e97c210f4704
User <User@DESKTOP-FOENCO9>
2022-02-21 09:42:43 +0530

    first_change Jayshri

--- Z_git_jenkins/src/Enquiry/Enquiry_client_info.java
+++ Z_git_jenkins/src/Enquiry/Enquiry_client_info.java
@@ -5,6 +5,7 @@ public class Enquiry_client_info {
 	public static void main(String[] args) {
 		
 		System.out.println("This is Enquiry Module");
+		System.out.println("Collecting information of client");
 		
 	}
 }
